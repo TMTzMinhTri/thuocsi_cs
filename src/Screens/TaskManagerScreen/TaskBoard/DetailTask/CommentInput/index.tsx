f719831a127889feef9d8c267ec00ac9553cf35b
Trần Minh Trí <tmtzminhtri@gmail.com>
2020-08-06 11:34:43 +0700

    style

--- src/Screens/TaskManagerScreen/TaskBoard/DetailTask/CommentInput/index.tsx
+++ src/Screens/TaskManagerScreen/TaskBoard/DetailTask/CommentInput/index.tsx
@@ -13,50 +13,61 @@ interface IProps {
 
 
 export const CommentInput: React.FC<IProps> = ({ name, CreateComment, currentUser }) => {
+    const [isInput, setIsInput] = React.useState<boolean>(false)
     let formRef = React.useRef<FormikProps<{ comment: string }>>(null)
+    let inputRef = React.useRef<HTMLElement>(null)
+
     const submitLogin = (values: { comment: string }, { resetForm, setSubmitting }: FormikHelpers<{ comment: string }>) => {
         if (isEmpty(values.comment)) {
             setSubmitting(false)
-
             return
         }
         CreateComment(values.comment, () => {
             resetForm({})
             setSubmitting(false)
+            setIsInput(false)
         })
     }
+    const toggleInput = () => {
+        setIsInput(!isInput)
+    }
+
+    React.useEffect(() => {
+        if (isInput === true && null !== inputRef.current) inputRef.current.focus()
+    }, [isInput])
+
     React.useEffect(() => {
         if (!isEmpty(formRef.current?.values.comment)) formRef.current?.resetForm()
     }, [name, formRef])
 
-    return <React.Fragment>
-        <FormGroup>
-            <Label className="detail-task__title" for={`user_${currentUser?.id}`}>Activity</Label>
-            <div className="detail-task__comment">
-                <Components.Avata name={currentUser ? currentUser.name : ""} target={`current_user_${currentUser?.id}`} classNames="mr-2" />
-                <Formik
+    return <FormGroup>
+        <Label className="detail-task__title" for={`user_${currentUser?.id}`}>Activity</Label>
+        <div className="detail-task__comment">
+            <Components.Avata name={currentUser ? currentUser.name : ""} target={`current_user_${currentUser?.id}`} classNames="mr-2" />
+            {isInput
+                ? <Formik
                     innerRef={formRef}
                     initialValues={{ comment: "" }}
-                    onReset={() => { }}
-                    onSubmit={submitLogin}
-                >
+                    onSubmit={submitLogin}>
                     {({ isSubmitting }) => (
-                        <Form>
+                        <Form className="w-100">
                             <Field name="comment"
-                                type="text"
+                                type="textarea"
+                                innerRef={inputRef}
                                 id={`user_${currentUser?.id}`}
                                 component={Components.ReactstrapFormik.BaseInput}
-                                placeholder="Số điện thoại" />
+                                placeholder="Nhập comment ..." />
 
-                            <Button color="primary" block
+                            <Button color="primary"
+                                className="mr-2"
                                 disabled={isSubmitting}
-                                type="submit">
-                                SIGN IN
-            </Button>
+                                type="submit">Lưu</Button>
+                            <Button
+                                type="button" onClick={toggleInput}>Huỷ</Button>
                         </Form>
                     )}
                 </Formik>
-            </div>
-        </FormGroup>
-    </React.Fragment>
+                : <div onClick={(toggleInput)} className="detail-task__comment-placeholder">Add a comment...</div>}
+        </div>
+    </FormGroup>
 }
