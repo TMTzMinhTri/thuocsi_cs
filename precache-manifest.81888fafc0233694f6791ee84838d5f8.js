2d24ed43d3b54e29bdf72eaebd4ef144c2b71907
Trần Minh Trí <tmtzminhtri@gmail.com>
2020-08-07 09:57:56 +0700

    deploy

---
+++ precache-manifest.81888fafc0233694f6791ee84838d5f8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7404f01cdab1fecd2eaa85920e1d7115",
+    "url": "/thuocsi_cs/index.html"
+  },
+  {
+    "revision": "a27040f24f56b38d33e9",
+    "url": "/thuocsi_cs/static/css/3.1c5ef917.chunk.css"
+  },
+  {
+    "revision": "7e649f0f855f98b4cc3a",
+    "url": "/thuocsi_cs/static/css/4.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "13bab3688b44afaba094",
+    "url": "/thuocsi_cs/static/css/main.c4242f1a.chunk.css"
+  },
+  {
+    "revision": "6776fa39f9d58d98bd29",
+    "url": "/thuocsi_cs/static/js/0.14409be3.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/thuocsi_cs/static/js/0.14409be3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27040f24f56b38d33e9",
+    "url": "/thuocsi_cs/static/js/3.f0b8e36f.chunk.js"
+  },
+  {
+    "revision": "087c1abdbc826c690da79d7944333072",
+    "url": "/thuocsi_cs/static/js/3.f0b8e36f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e649f0f855f98b4cc3a",
+    "url": "/thuocsi_cs/static/js/4.74265b9f.chunk.js"
+  },
+  {
+    "revision": "88a7ef7c9cb54df7ebd0",
+    "url": "/thuocsi_cs/static/js/5.cf9cede9.chunk.js"
+  },
+  {
+    "revision": "781ca36f3cf55de56142",
+    "url": "/thuocsi_cs/static/js/6.b626f4a1.chunk.js"
+  },
+  {
+    "revision": "13bab3688b44afaba094",
+    "url": "/thuocsi_cs/static/js/main.ba3a1175.chunk.js"
+  },
+  {
+    "revision": "0813478c965ad3f9398c",
+    "url": "/thuocsi_cs/static/js/runtime-main.053a8db2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/thuocsi_cs/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/thuocsi_cs/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/thuocsi_cs/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/thuocsi_cs/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/thuocsi_cs/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "96e06b433366e093b6e1613f8dc88702",
+    "url": "/thuocsi_cs/static/media/logo_thuocsi.96e06b43.svg"
+  }
+]);
