07fd62873cdbaa24f15f05ac23327f64765e07e1
Trần Minh Trí <tmtzminhtri@gmail.com>
2020-08-12 16:58:14 +0700

    add modal

---
+++ src/Screens/TaskManagerScreen/FilterControl/SelectCreater/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { FormGroup, Label } from 'reactstrap';
+import { IResponseUser } from 'Interface/Response/session.types';
+import Select from 'react-select'
+
+interface ISelectCreaterProps {
+    listMember: IResponseUser[],
+    handleOnSelect: (value: { type: string, value: any }) => void
+    created_by: { label: string; value: number }
+}
+
+export const SelectCreater: React.FC<ISelectCreaterProps> = ({ listMember, created_by, handleOnSelect }) => {
+    return <FormGroup>
+        <Label >Creater</Label>
+        <Select
+            value={created_by}
+            onChange={(value) => handleOnSelect({ type: "created_by_id", value })}
+            options={listMember.map(item => ({ label: item.name, value: item.id }))}
+        />
+    </FormGroup>
+}
--- src/Screens/TaskManagerScreen/TaskBoard/DetailTask/index.tsx
+++ src/Screens/TaskManagerScreen/TaskBoard/DetailTask/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import Utils from "Utils";
 import { Icstasks } from 'Interface/Response/task_manager.types';
-import { CardTitle, CardText, Badge } from 'reactstrap';
+import { CardTitle, CardText, Badge, Modal, ModalHeader, ModalBody } from 'reactstrap';
 // import * as Components from "Components";
 import { connect } from 'react-redux';
 import { RootState } from 'Store';
@@ -42,43 +42,48 @@ export const DetailTaskComponent: React.FC<Iprops> = React.memo(({ path, task_ma
 
 
     return selected_task && task_manager.task_selected !== null
-        ? <div ref={refDetail} className="detail-task">
-            <div className="detail-task__header">
-                <div>{Utils.FormatDateBy_YYYY_MM_DD(task_manager.task_selected?.created_at)} - {Utils.converTime(task_manager.task_selected?.created_at)}</div>
-                <Link to={path}>
-                    <i className="fa fa-times" aria-hidden="true"></i>
-                </Link>
-            </div>
-            <div className="detail-task__body">
-                <div className="detail-task__title">{task_manager.task_selected?.cs_note}</div>
-                <div className="my-2">
-                    {task_manager.task_selected?.failure_type_names.map((item, index) =>
-                        <Badge
-                            color="primary"
-                            pill
-                            className={index !== 0 ? "mx-1" : ""}
-                            key={`failure_type_${index}`}>{item}</Badge>)}
+        ? <Modal isOpen={selected_task} toggle={() => { }} className="">
+            <ModalHeader toggle={() => { }}>Modal title</ModalHeader>
+            <ModalBody>
+                <div ref={refDetail} className="detail-task">
+                    <div className="detail-task__header">
+                        <div>{Utils.FormatDateBy_YYYY_MM_DD(task_manager.task_selected?.created_at)} - {Utils.converTime(task_manager.task_selected?.created_at)}</div>
+                        <Link to={path}>
+                            <i className="fa fa-times" aria-hidden="true"></i>
+                        </Link>
+                    </div>
+                    <div className="detail-task__body">
+                        <div className="detail-task__title">{task_manager.task_selected?.cs_note}</div>
+                        <div className="my-2">
+                            {task_manager.task_selected?.failure_type_names.map((item, index) =>
+                                <Badge
+                                    color="primary"
+                                    pill
+                                    className={index !== 0 ? "mx-1" : ""}
+                                    key={`failure_type_${index}`}>{item}</Badge>)}
+                        </div>
+                        <div>
+                            <CardTitle>Thông tin đơn hàng</CardTitle>
+                            <CardText>G/T đơn hàng: {Utils.formatCurrency(task_manager.task_selected.total)}</CardText>
+                            <CardText>S/L sản phẩm{task_manager.task_selected.quantity_counter}</CardText>
+                            <CardText>{task_manager.task_selected.so_id}</CardText>
+                            <CardText>{task_manager.task_selected.order_id}</CardText>
+                            <CardText>{task_manager.task_selected.order_status}</CardText>
+                            <CardText>{task_manager.task_selected.return_id}</CardText>
+                            <CardText >Mã return: {task_manager.task_selected.return_id}</CardText>
+                            <CardTitle>Thông tin Khách hàng</CardTitle>
+                            <CardText>Nhà thuốc: {task_manager.task_selected.business_name}</CardText>
+                            <CardText>Khách hàng: {task_manager.task_selected.user_name}</CardText>
+                            <CardText>SĐT: {task_manager.task_selected.user_phone}</CardText>
+                        </div>
+                        <div className="detail-task__activity" >
+                            <CommentInput name={selected_task} CreateComment={createCommentInTask} currentUser={currentUser} comments={task_manager.task_selected.comments} />
+                            <CommentList comments={task_manager.task_selected.comments} />
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <CardTitle>Thông tin đơn hàng</CardTitle>
-                    <CardText>G/T đơn hàng: {Utils.formatCurrency(task_manager.task_selected.total)}</CardText>
-                    <CardText>S/L sản phẩm{task_manager.task_selected.quantity_counter}</CardText>
-                    <CardText>{task_manager.task_selected.so_id}</CardText>
-                    <CardText>{task_manager.task_selected.order_id}</CardText>
-                    <CardText>{task_manager.task_selected.order_status}</CardText>
-                    <CardText>{task_manager.task_selected.return_id}</CardText>
-                    <CardText >Mã return: {task_manager.task_selected.return_id}</CardText>
-                    <CardTitle>Thông tin Khách hàng</CardTitle>
-                    <CardText>Nhà thuốc: {task_manager.task_selected.business_name}</CardText>
-                    <CardText>Khách hàng: {task_manager.task_selected.user_name}</CardText>
-                    <CardText>SĐT: {task_manager.task_selected.user_phone}</CardText>
-                </div>
-                <div className="detail-task__activity" >
-                    <CommentInput name={selected_task} CreateComment={createCommentInTask} currentUser={currentUser} comments={task_manager.task_selected.comments} />
-                    <CommentList comments={task_manager.task_selected.comments} />
-                </div>
-            </div>
-        </div>
+            </ModalBody>
+        </Modal>
         : null
 })
 export const DetailTask = connect(mapState, mapAction)(withRouter(DetailTaskComponent))
